ec8d4d93ee312c9647d3a86f275b54695c4a7b28
Ronish Shrestha <157086816+RonishS@users.noreply.github.com>
2026-01-31 22:13:48 +0545

    Update JdbcMetadataDemo.java

--- src/lab2/JdbcMetadataDemo.java
+++ src/lab2/JdbcMetadataDemo.java
@@ -9,9 +9,9 @@ public class JdbcMetadataDemo {
 
     public static void main(String[] args) {
 
-        String url = "jdbc:mysql://localhost:3306/lab2db";
-        String user = "root";
-        String password = "";
+String url = "jdbc:mysql://localhost:3306/YOUR_DATABASE_NAME";
+String user = "YOUR_USERNAME";
+String password = "YOUR_PASSWORD";
 
        
         try (Connection con = DriverManager.getConnection(url, user, password)) {
